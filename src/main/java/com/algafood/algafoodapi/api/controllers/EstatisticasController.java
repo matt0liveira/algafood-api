55fce9b03eb56e6b8bbd9437f4e854f121e98613
Matheus Oliveira <matheus.4004.mo33@gmail.com>
2022-09-29 21:02:17 -0300

    commit com aula 13.15 finalizada

--- src/main/java/com/algafood/algafoodapi/api/controllers/EstatisticasController.java
+++ src/main/java/com/algafood/algafoodapi/api/controllers/EstatisticasController.java
@@ -1,10 +1,26 @@
 package com.algafood.algafoodapi.api.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.algafood.algafoodapi.domain.filter.VendaDiariaFilter;
+import com.algafood.algafoodapi.domain.models.dto.VendaDiaria;
+import com.algafood.algafoodapi.infrastructure.service.VendaQueryServiceImpl;
 
 @RestController
 @RequestMapping("/estatisticas")
 public class EstatisticasController {
     
+    @Autowired
+    private VendaQueryServiceImpl vendaQueryService;
+
+    @GetMapping("/vendas-diarias")
+    public List<VendaDiaria> consultVendasDiarias(VendaDiariaFilter filter) {
+        return vendaQueryService.consultVendasDiarias(filter);
+    }
+
 }
