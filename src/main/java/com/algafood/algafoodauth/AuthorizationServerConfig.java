daa8edf79e4f76c256a2194c2e0c4357db2176a5
Matheus Oliveira <matheus.4004.mo33@gmail.com>
2022-10-28 23:06:06 -0300

    Authorization Server - aula 22.19 finalizada

---
+++ src/main/java/com/algafood/algafoodauth/AuthorizationServerConfig.java
@@ -0,0 +1,72 @@
+package com.algafood.algafoodauth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        // CONFIGURANDO CLIENT
+        clients.inMemory()
+                .withClient("algafood-web")
+                .secret(passwordEncoder.encode("web123"))
+                .authorizedGrantTypes("password", "refresh_token")
+                .scopes("write", "read")
+                .accessTokenValiditySeconds(60 * 60 * 6) // PADRÃO DE 12H
+                .refreshTokenValiditySeconds(60 * 60 * 24 * 60) // PADRÃO DE 30 DIAS
+
+                .and()
+                .withClient("faturamento")
+                .secret(passwordEncoder.encode("faturamento123"))
+                .authorizedGrantTypes("client_credentials")
+                .scopes("write", "read")
+
+                .and()
+                .withClient("foodanalytics")
+                .secret(passwordEncoder.encode("food123"))
+                .authorizedGrantTypes("authorization_code")
+                .scopes("write", "read")
+                .redirectUris("http://127.0.0.1:5500/")
+
+                .and()
+                .withClient("checktoken")
+                .secret(passwordEncoder.encode("check123"));
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+        // .reuseRefreshTokens(false); MÉTODO BOOLEANO QUE DETERMINA A REUTILIZAÇÃO DE
+        // UM REFRESH_TOKEN
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                // .checkTokenAccess("isAuthenticated()");
+                .checkTokenAccess("permitAll()");
+    }
+}
