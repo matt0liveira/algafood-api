afa84d8049abb7d549baef209d819b254e4da05e
Matheus Oliveira <matheus.4004.mo33@gmail.com>
2022-11-20 16:21:10 -0300

    commit com aula 26.13 finalizada

--- src/main/java/com/algafood/algafoodapi/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algafood/algafoodapi/core/springdoc/SpringDocConfig.java
@@ -2,6 +2,7 @@ package com.algafood.algafoodapi.core.springdoc;
 
 import java.util.Arrays;
 
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,8 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.tags.Tag;
 
 @Configuration
@@ -34,6 +37,59 @@ public class SpringDocConfig {
 								.url("http://springdoc.com")))
 				.tags(Arrays.asList(
 						new Tag().name("Cidades").description("Gerencia as cidades")));
+	}
+
+	@Bean
+	public OpenApiCustomiser openApiCustomiser() {
+		return openapi -> {
+			openapi.getPaths()
+					.values()
+					.forEach(pathItem -> pathItem.readOperationsMap()
+							.forEach((httpMethod, operation) -> {
+								ApiResponses responses = operation.getResponses();
+
+								switch (httpMethod) {
+									case GET:
+										responses.addApiResponse("406",
+												new ApiResponse()
+														.description(
+																"Recurso não possui representação que poderia ser aceita pelo consumidor"));
+
+										responses.addApiResponse("500",
+												new ApiResponse().description("Erro interno do servidor"));
+										break;
+
+									case POST:
+										responses.addApiResponse("400",
+												new ApiResponse()
+														.description("Requisiçãi inválida do lado do consumidor"));
+
+										responses.addApiResponse("500",
+												new ApiResponse().description("Erro interno do servidor"));
+
+										break;
+
+									case PUT:
+										responses.addApiResponse("400",
+												new ApiResponse()
+														.description("Requisiçãi inválida do lado do consumidor"));
+
+										responses.addApiResponse("500",
+												new ApiResponse().description("Erro interno do servidor"));
+										break;
+
+									case DELETE:
+										responses.addApiResponse("500",
+												new ApiResponse().description("Erro interno do servidor"));
+
+										break;
+
+									default:
+										break;
+								}
+							}));
+			;
+		};
 	}
 
 }
